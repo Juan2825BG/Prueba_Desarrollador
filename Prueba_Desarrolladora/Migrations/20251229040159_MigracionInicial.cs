46f5dd86b48aefe155d1d39bfe563f49113baf48
Juan2825BG <jpbg2825@gmail.com>
2025-12-28 23:06:00 -0500

    .

---
+++ Prueba_Desarrolladora/Migrations/20251229040159_MigracionInicial.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prueba_Desarrollador.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autores",
+                columns: table => new
+                {
+                    AutorID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Autores__F58AE9091F2A03EA", x => x.AutorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Libros",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    AutorID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Libros__3214EC2725226FA1", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Libros_Autores",
+                        column: x => x.AutorID,
+                        principalTable: "Autores",
+                        principalColumn: "AutorID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Libros_AutorID",
+                table: "Libros",
+                column: "AutorID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Libros");
+
+            migrationBuilder.DropTable(
+                name: "Autores");
+        }
+    }
+}
